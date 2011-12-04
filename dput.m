a0873f93662fa42cbee8132c64d3975d62f26d15
John Colby <johncolby@ucla.edu>
2011-12-04 15:21:33 -0800

    Initial support for structures

--- dput.m
+++ dput.m
@@ -16,6 +16,20 @@ for i=1:nargin
                celldata{icell} = dput(vardata{icell});
            end
            pastedata{i} = ['reshape({' sprintf('%s ', celldata{:}) '},' '[' num2str(size(vardata)) '])'];
+       case 'struct'
+           fields = fieldnames(vardata);
+           fielddata = cell(length(fields), 1);
+           for ifield=1:length(fields)
+               vardata2 = {vardata.(fields{ifield})};
+               celldata = cell(length(fields), 1);
+               for icell=1:length(vardata2(:))
+                   celldata{icell} = dput(vardata2{icell});
+               end
+               fielddata{ifield} = ['reshape({' sprintf('%s ', celldata{:}) '},' '[' num2str(size(vardata2)) '])'];
+           end
+           fielddata = [fields fielddata]';
+           fielddata = ['struct(' sprintf('''%s'',%s,', fielddata{:})];
+           pastedata{i} = [fielddata(1:(end-1)) ')'];
        otherwise
            pastedata{i} = '[]';
            warning(sprintf('''%s'' not written. Class ''%s'' not supported.', inputname(i), class(vardata)))
