932ca6a75407417586b71081cb1ae7a1b8742768
John Colby <johncolby@ucla.edu>
2011-12-04 13:35:05 -0800

    Initial support for cells; Changed input args to pass actual variables

--- dput.m
+++ dput.m
@@ -1,19 +1,30 @@
-function pastedata = dput(vars)
+function pastedata = dput(varargin)
 
-pastedata = cell(length(vars), 1);
+pastedata = cell(nargin, 1);
 
-for i=1:length(vars)
-   vardata = evalin('caller', vars{i});
+for i=1:nargin
+   vardata = varargin{i};
    
    switch class(vardata)
        case 'double'
-           pastedata{i} = [vars{i} '=' 'reshape([' sprintf('%f ', vardata) '],' '[' num2str(size(vardata)) ']);'];
+           pastedata{i} = ['reshape([' sprintf('%f ', vardata) '],' '[' num2str(size(vardata)) '])'];
        case 'char'
-           pastedata{i} = [vars{i} '=' 'reshape(''' sprintf('%s', vardata) ''',' '[' num2str(size(vardata)) ']);'];
+           pastedata{i} = ['reshape(''' sprintf('%s', vardata) ''',' '[' num2str(size(vardata)) '])'];
+       case 'cell'
+           celldata = cell(length(vardata(:)), 1);
+           for icell=1:length(vardata(:))
+               celldata{icell} = dput(vardata{icell});
+           end
+           pastedata{i} = ['reshape({' sprintf('%s ', celldata{:}) '},' '[' num2str(size(vardata)) '])'];
        otherwise
            pastedata{i} = '';
            warning(sprintf('''%s'' not written. Class ''%s'' not supported.', vars{i}, class(vardata)))
    end
 end
 
+varnames = char(arrayfun(@inputname, 1:nargin, 'UniformOutput', false));
+
 pastedata = char(pastedata);
+if varnames
+    pastedata = [varnames repmat(' = ', nargin, 1) pastedata repmat(';', nargin, 1)];
+end
