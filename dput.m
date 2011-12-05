cc144a4ead5dec79d2edd8eba0eb367bb6f613af
John Colby <johncolby@ucla.edu>
2011-12-04 16:38:32 -0800

    Replaced some redundant code with recursion

--- dput.m
+++ dput.m
@@ -62,12 +62,7 @@ for i=1:nargin
            fields = fieldnames(vardata);
            fielddata = cell(length(fields), 1);
            for ifield=1:length(fields)
-               vardata2 = {vardata.(fields{ifield})};
-               celldata = cell(length(fields), 1);
-               for icell=1:length(vardata2(:))
-                   celldata{icell} = dput(vardata2{icell});
-               end
-               fielddata{ifield} = ['reshape({' sprintf('%s ', celldata{:}) '},' '[' num2str(size(vardata2)) '])'];
+               fielddata{ifield} = dput({vardata.(fields{ifield})});
            end
            fielddata = [fields fielddata]';
            fielddata = ['struct(' sprintf('''%s'',%s,', fielddata{:})];
